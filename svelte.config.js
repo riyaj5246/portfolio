efc6c06aba5302ae25d8e75717c311f8e23afc6c
Riya Jain <riyajj2@illinois.edu>
2023-06-11 12:19:24 -0500

    svelte config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: "nodejs18.x",
+    }),
   },
 
   preprocess: [
